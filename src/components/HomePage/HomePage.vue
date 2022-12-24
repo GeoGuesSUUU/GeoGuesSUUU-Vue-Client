e66ead9473f6ae0f885233a7f75d93ef1ea88355
DRACOX <alexis.forestier2001@gmail.com>
2022-12-24 16:40:35 +0100

    move script + HomePage

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-	<p>That Works !</p>
+	<div class="px-4 pt-5 my-5 text-center border-bottom">
+		<h1 class="display-4 fw-bold">World Map</h1>
+		<div class="col-lg-6 mx-auto">
+			<p class="lead mb-4">Buy, protect and attack countries in the world !</p>
+		</div>
+		<div class="overflow-hidden" style="max-height: 30vh">
+			<div class="card px-5 border rounded-3 shadow-lg">
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped src="./HomePage.scss"></style>
