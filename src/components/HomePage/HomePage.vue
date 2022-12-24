435c37e39b3eef2e9b9b9d227871e0df2770c065
DRACOX <alexis.forestier2001@gmail.com>
2022-12-24 18:16:10 +0100

    add world map

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -1,4 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import svgMap from 'svgmap'
+import { onMounted, ref } from 'vue'
+
+const map = ref(null)
+
+onMounted(() => {
+	map.value = new svgMap({
+		targetElementID: 'svgMap',
+		showContinentSelector: false,
+		showZoomReset: true,
+		mouseWheelZoomEnabled: true,
+		hideFlag: false,
+		data: {
+			data: {
+				owner: {
+					name: 'Owner',
+					format: '{0}',
+					// thousandSeparator: ',',
+					// thresholdMax: 50000,
+					// thresholdMin: 1000,
+					// color: '#ccc',
+				},
+				// change: {
+				//   name: "Change to year before",
+				//   format: "{0} %",
+				// },
+			},
+			applyData: 'owner',
+			values: {
+				FR: { owner: 'Admin', color: '#F00' },
+				// AF: { countresidents: 587 },
+				// AL: { gdp: 4583, change: 11.09 },
+				// DZ: { countresidents: 232 },
+				// UA: { countresidents: 42393 },
+				// ...
+			},
+		},
+	})
+})
+</script>
 
 <template>
 	<div class="px-4 pt-5 my-5 text-center border-bottom">
@@ -6,8 +46,9 @@
 		<div class="col-lg-6 mx-auto">
 			<p class="lead mb-4">Buy, protect and attack countries in the world !</p>
 		</div>
-		<div class="overflow-hidden" style="max-height: 30vh">
+		<div class="mb-5">
 			<div class="card px-5 border rounded-3 shadow-lg">
+				<div id="svgMap" ref="map"></div>
 			</div>
 		</div>
 	</div>
