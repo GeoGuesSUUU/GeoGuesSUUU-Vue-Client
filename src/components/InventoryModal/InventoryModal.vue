67281cf4695bc027f66aa576e62dd5f5f6809e44
DRACOX <alexis.forestier2001@gmail.com>
2023-01-05 23:42:39 +0100

    fix inventory responsive

--- src/components/InventoryModal/InventoryModal.vue
+++ src/components/InventoryModal/InventoryModal.vue
@@ -140,7 +140,8 @@ eventEmitter.on(
 						<option value="equipment">Equipment</option>
 					</select>
 					<hr class="hr" />
-					<div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4 inventory">
+					<div
+						class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5 g-4 inventory">
 						<div
 							v-for="item of items"
 							:key="item.itemType.id"
